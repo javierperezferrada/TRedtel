616ae6d99381a56a68b9ba7a8e99a4a44911ef01
javier perez ferrada <javierperezferrada@gmail.com>
2015-11-10 13:07:58 -0300

    carga de usuarios y liquidaciones lista, muestra liquidacion pero falta hacer una query mas optima

--- projects/dev_portalRedtel/portal/models.py
+++ projects/dev_portalRedtel/portal/models.py
@@ -1,23 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
 
-class Document(models.Model):
-    docfile = models.FileField(upload_to='/home/javier/TRedtel/projects/dev_portalRedtel/media')
-#clase de prueba carga de csv
-class Palabra(models.Model):
-	tipo = models.CharField(max_length=20)
-	palabra1 = models.CharField(max_length=20)
-	palabra2 = models.CharField(max_length=20)
-#fin clase de prueba
-
-class Usuario(models.Model):
-	nombre = models.CharField(max_length=20)
-	rut = models.CharField(max_length=20)
-
-#----------------------------------------------------
-# Clases propuestas por el cliente.
-#----------------------------------------------------
 class Cargo(models.Model):
 	nombre = models.CharField(max_length=30)
 
@@ -33,14 +18,24 @@ class Area(models.Model):
 class CentroCosto(models.Model):
 	nombre = models.CharField(max_length=30)
 
-#----------------------------------------------------
-# Fin Clases propuestas por el cliente.
-#----------------------------------------------------
+class Usuario(models.Model):
+    User_id = models.IntegerField()
+    rut = models.CharField(max_length=45)
+    fecha_ingreso = models.DateField()
+    Area_id = models.IntegerField()
+    Afp_id = models.IntegerField()
+    Salud_id = models.IntegerField()
+    CentroCosto_id = models.IntegerField()
+    vencimiento_licencia_conducir = models.DateField()
+    Cargo_id = models.IntegerField()
 
 class Liquidacion(models.Model):
-	rut_trabajador = models.CharField(max_length=12)
+	Usuario_rut = models.CharField(max_length=45)
 	mes = models.IntegerField()
 	ano = models.IntegerField()
+	porcentaje_cotizacion = models.IntegerField()
+	pactado = models.IntegerField()
+	dias_trabajados = models.IntegerField()
 	sueldo = models.IntegerField()
 	gratificacion = models.IntegerField()
 	comision_produccion = models.IntegerField()
@@ -50,22 +45,12 @@ class Liquidacion(models.Model):
 	afp = models.IntegerField()
 	adiciona_afp = models.IntegerField()
 	salud = models.IntegerField()
-	seg_cesantia = models.IntegerField()
+	seguro_cesantia = models.IntegerField()
 	anticipo = models.IntegerField()
-	ant_combustible = models.IntegerField()
-	ant_viatico = models.IntegerField()
+	anticipo_combustible = models.IntegerField()
+	anticipo_viatico = models.IntegerField()
 
-class Trabajador(models.Model):
-	nombre = models.CharField(max_length=50)
-	direccion = models.CharField(max_length=150)
-	id_cargo = models.ForeignKey(Cargo) 
-	ingreso = models.DateField()
-	id_afp = models.ForeignKey(Afp) 
-	id_salud = models.ForeignKey(Salud) 
-	rut = models.CharField(max_length=12)
-	f_nacimiento = models.DateField()
-	id_area = models.ForeignKey(Area) 
-	id_ccosto = models.ForeignKey(CentroCosto) 
-	vencimiento_lic_conducir = models.DateField()
+
+
 
 
--- projects/dev_portalRedtel/portal/views.py
+++ projects/dev_portalRedtel/portal/views.py
@@ -9,8 +9,6 @@ from django.template.context import RequestContext
 from django.contrib.auth.decorators import login_required,permission_required
 from .models import Usuario
 from .models import Liquidacion
-from .models import Palabra
-from .models import Document
 from django.contrib import messages
 import csv
 from .forms import UploadFileForm
@@ -25,14 +23,24 @@ def home(request):
 
 @login_required()
 def mis_datos(request):
-	usuario = get_object_or_404(Usuario, nombre=request.user.username)
+	usuario = get_object_or_404(Usuario, id=request.user.id)
 	return render_to_response('mis_datos.html', {'usuario': usuario}, context_instance=RequestContext(request))
 
 @login_required()
+def obtener_certificado(request):
+    usuario = get_object_or_404(Usuario, id=request.user.id)
+    return render_to_response('obtener_certificado.html', {'usuario': usuario}, context_instance=RequestContext(request))
+
+@login_required()
 def mis_liquidaciones(request):
-	usuario = get_object_or_404(Usuario, nombre=request.user.username)
-	liquidacion = get_object_or_404(Liquidacion, rut_trabajador=usuario.rut)
-	return render_to_response('mis_liquidaciones.html', {'usuario': usuario}, context_instance=RequestContext(request))
+	usuario = get_object_or_404(Usuario, id=request.user.id)
+	liquidacion = get_object_or_404(Liquidacion, Usuario_rut=usuario.rut)
+	return render_to_response('mis_liquidaciones.html', {'liquidacion': liquidacion}, context_instance=RequestContext(request))
+
+@login_required()
+def copias_liquidaciones(request):
+    usuario = get_object_or_404(Usuario, id=request.user.id)
+    return render_to_response('copias_liquidaciones.html', {'usuario': usuario}, context_instance=RequestContext(request))
 
 @permission_required('portal.puede_cargar', login_url="/ingresar") 
 def cargar_usuarios(request):	
@@ -41,19 +49,62 @@ def cargar_usuarios(request):
         if form.is_valid():
             reader = csv.reader(request.FILES['docfile'])
             for row in reader:
-            	user = User.objects.create_user(username=row[1],password=row[3]) 
-                usuarios = Usuario()
-                usuarios.id = row[0]
-                usuarios.nombre = row[1]
-                usuarios.rut = row[2]
-                usuarios.save()
-            return HttpResponseRedirect('/')
+                if row[0]=='id':
+                    print 'cabecera'
+                else:
+                    user = User.objects.create_user(id=row[0],
+                        username=row[1],first_name = row[2],
+                        last_name=row[3],email=row[4],password=row[5]) 
+                    usuarios = Usuario()
+                    usuarios.id = row[0]
+                    usuarios.User_id = row[0]
+                    usuarios.rut = row[7]
+                    usuarios.fecha_ingreso = row[8]
+                    usuarios.Area_id = int(row[9])
+                    usuarios.Afp_id = row[10]
+                    usuarios.Salud_id = row[11]
+                    usuarios.CentroCosto_id = row[12]
+                    usuarios.vencimiento_licencia_conducir = row[13]
+                    usuarios.Cargo_id = row[14]
+                    usuarios.save()
+            return HttpResponseRedirect('/home')
     else:
         form = UploadFileForm()
     return render_to_response('cargar_usuarios.html', {'form': form}, context_instance=RequestContext(request))
 
 @permission_required('portal.puede_cargar', login_url="/ingresar")  
 def cargar_liquidaciones(request):
-	return render_to_response('cargar_liquidaciones.html', context_instance=RequestContext(request))
-
-
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            reader = csv.reader(request.FILES['docfile'])
+            for row in reader:
+                if row[0]=='usuario_rut':
+                    print 'cabecera'
+                else: 
+                    liquidaciones = Liquidacion()
+                    liquidaciones.Usuario_rut = row[0]
+                    liquidaciones.mes = row[1]
+                    liquidaciones.ano = row[2]
+                    liquidaciones.porcentaje_cotizacion = row[3]
+                    liquidaciones.pactado = row[4]
+                    liquidaciones.tributable = row[5]
+                    liquidaciones.dias_trabajados = row[6]
+                    liquidaciones.sueldo = row[7]
+                    liquidaciones.gratificacion = row[8]
+                    liquidaciones.comision_produccion = row[9]
+                    liquidaciones.semana_corrida = row[10]
+                    liquidaciones.asignacion_viaticos = row[11]
+                    liquidaciones.movilizacion_combustible = row[12]
+                    liquidaciones.afp = row[13]
+                    liquidaciones.adiciona_afp = row[14]
+                    liquidaciones.salud = row[15]
+                    liquidaciones.seguro_cesantia = row[16]
+                    liquidaciones.anticipo = row[17]
+                    liquidaciones.anticipo_combustible = row[18]
+                    liquidaciones.anticipo_viatico = row[19]
+                    liquidaciones.save()
+            return HttpResponseRedirect('/home')
+    else:
+        form = UploadFileForm()
+    return render_to_response('cargar_liquidaciones.html', {'form': form}, context_instance=RequestContext(request))
