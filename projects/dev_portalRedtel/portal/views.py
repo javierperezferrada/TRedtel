cde57541bf8704a2e7718282534898da3561ab08
javierperezferrada <javier_dc2@hotmail.com>
2015-11-25 18:44:43 -0300

    Update views.py

--- projects/dev_portalRedtel/portal/views.py
+++ projects/dev_portalRedtel/portal/views.py
@@ -27,15 +27,11 @@ def index(request):
 def home(request):
    return render_to_response('home.html', {'user': request.user}, context_instance=RequestContext(request))
 
-@login_required()
-def us(request):
-    us = Us.objects.get(id=1)
-    return render_to_response('us.html', {'us': us}, context_instance=RequestContext(request))
 
 
 @login_required()
 def mis_datos(request):
-	usuario = get_object_or_404(Usuario, id=request.user.id)
+	usuario = get_object_or_404(Usuario, rut=request.user.first_name)
 	return render_to_response('mis_datos.html', {'usuario': usuario}, context_instance=RequestContext(request))
 
 #@login_required()
@@ -157,25 +153,35 @@ def cargar_usuarios(request):
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             reader = csv.reader(request.FILES['docfile'])
-            for row in reader:
-                if row[0]=='id':
+            index = 0
+            for index,row in enumerate(reader):
+                if index<7:
                     print 'cabecera'
-                else:
-                    user = User.objects.create_user(id=row[0],
-                        username=row[1],first_name = row[2],
-                        email=row[4],password=row[5]) 
+                else: 
                     usuarios = Usuario()
-                    usuarios.id = row[0]
-                    usuarios.User_id = row[0]
-                    usuarios.rut = row[7]
-                    usuarios.fecha_ingreso = row[8]
-                    usuarios.area = row[9]
-                    usuarios.afp = row[10]
-                    usuarios.salud = row[11]
-                    usuarios.ccosto = row[12]
-                    usuarios.vencimiento_licencia_conducir = row[13]
-                    usuarios.cargo = row[14]
-                    usuarios.direccion = row[15]
+                    usuarios.nombre = row[1]
+                    usuarios.rut = row[4]
+                    usuarios.zonal = row[2]
+                    usuarios.afp =row[18]
+                    usuarios.salud = row[19]
+                    usuarios.ccosto = row[3]
+                    usuarios.cargo = row[29]
+                    usuarios.fecha_ingreso = row[16]
+                    usuarios.direccion = row[5]
+                    usuarios.comuna = row[6]
+                    usuarios.celular = row[7]
+                    usuarios.telefono = row[8]
+                    usuarios.f_nac = row[9]
+                    usuarios.correo = row[10]
+                    usuarios.est_civil = row[11]
+                    usuarios.nacionalidad = row[12]
+                    usuarios.licencia = row[13]
+                    usuarios.cargas_fam = row[14]
+                    usuarios.n_hijos = row[15]
+                    usuarios.f_contrato = row[17]
+                    usuarios.vencimiento = row[20]
+                    usuarios.tipo_pago = row[21]
+                    usuarios.n_cuenta = row[22]
                     usuarios.save()
             return HttpResponseRedirect('/home')
     else:
@@ -195,7 +201,7 @@ def cargar_liquidaciones(request):
                 else: 
                     liquidaciones = Liquidacion()
                     liquidaciones.Usuario_rut = row[1]
-                    liquidaciones.mes = 'mayo'
+                    liquidaciones.mes = 'junio'
                     liquidaciones.ano = 2015
                     liquidaciones.zonal = row[2]
                     liquidaciones.c_costo = row[3]
@@ -230,3 +236,16 @@ def cargar_liquidaciones(request):
     else:
         form = UploadFileForm()
     return render_to_response('cargar_liquidaciones.html', {'form': form}, context_instance=RequestContext(request))
+
+
+@login_required()
+def us(request):
+    with open('us_rut.csv', 'rb') as csvfile:
+        spamreader = csv.reader(csvfile)
+        for row in spamreader:
+            user = Us()
+            user.username = row[0]
+            user.password = row[2]
+            user.rut = row[1]
+            user.save()
+
